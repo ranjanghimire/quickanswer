a26349e1095d83cd6ff54ee299356704eb8506d5
Rolex Brown <ranjanghimire@gmail.com>
2016-12-11 12:32:52 -0600

    Added all CRUD operations for Question

--- src/main/java/com/gmire/controller/QuestionController.java
+++ src/main/java/com/gmire/controller/QuestionController.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.gmire.model.Question;
 import com.gmire.repository.QuestionRepository;
 import com.gmire.service.QuestionService;
@@ -23,7 +23,40 @@ public class QuestionController {
 
 	@Autowired
 	QuestionRepository qRepo;
+	
+	//Update a question
+	@RequestMapping(value="question/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Question> updateGreeting(@RequestBody Question question){
+		Question updateQuestion = questionService.update(question);
+		if (updateQuestion == null) {
+			return new ResponseEntity<Question> (HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<Question> (updateQuestion, HttpStatus.OK);
+	}
 
+	//Delete All Questions
+	@RequestMapping(value = "question", method = RequestMethod.DELETE)
+	public ResponseEntity<Question> deleteAllQuestions() {
+		questionService.deleteAll();
+		return new ResponseEntity<Question>(HttpStatus.NO_CONTENT);
+	}
+
+	// Delete a question by id
+	@RequestMapping(value = "question/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Question> deleteQuestion(@PathVariable("id") String id) {
+		questionService.delete(id);
+		return new ResponseEntity<Question>(HttpStatus.NO_CONTENT);
+	}
+
+	// Create a Question
+	@RequestMapping(value = "question", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
+		Question savedQuestion = questionService.create(question);
+		return new ResponseEntity<Question>(savedQuestion, HttpStatus.CREATED);
+	}
+
+	// Find all questions
 	@RequestMapping(value = "question", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Question>> findAll() {
 		List<Question> retQuestion = questionService.findAll();
@@ -33,13 +66,9 @@ public class QuestionController {
 		return new ResponseEntity<List<Question>>(retQuestion, HttpStatus.OK);
 	}
 
+	// Search by particular question
 	@RequestMapping(value = "question/{question}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Question>> findByMainQuestion(@PathVariable String question) {
-
-		qRepo.deleteAll();
-
-		qRepo.save(new Question("hello question"));
-		qRepo.save(new Question("testing"));
 
 		List<Question> retQuestion = questionService.findByMainQuestionIgnoreCaseLike(question);
 
--- src/main/java/com/gmire/service/QuestionService.java
+++ src/main/java/com/gmire/service/QuestionService.java
@@ -2,27 +2,48 @@ package com.gmire.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.gmire.model.Question;
 import com.gmire.repository.QuestionRepository;
 
 @Service
 public class QuestionService {
-	
+
 	@Autowired
 	private QuestionRepository qRepo;
-	
-	public List<Question> findByMainQuestionIgnoreCaseLike(String question){
+
+	public List<Question> findByMainQuestionIgnoreCaseLike(String question) {
 		List<Question> retList = new ArrayList<Question>();
 		retList = qRepo.findByMainQuestionIgnoreCaseLike(question);
 		return retList;
 	}
-	
-	public List<Question> findAll(){
+
+	public List<Question> findAll() {
 		return qRepo.findAll();
+	}
+
+	public Question create(Question question) {
+		Question savedQuestion = qRepo.save(question);
+		return savedQuestion;
+	}
+
+	public void delete(String id) {
+		qRepo.delete(id);
+	}
+
+	public void deleteAll() {
+		qRepo.deleteAll();
+	}
+
+	public Question update(Question question) {
+		Question questionPersisted = qRepo.findOne(question.getId());
+		if (questionPersisted == null) {
+			// cannot update question that hasn't been persisted
+			return null;
+		}
+		Question updateQuestion = qRepo.save(question);
+		return updateQuestion;
 	}
 
 }
